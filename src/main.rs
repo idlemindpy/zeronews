dbbdb8b31e04326073e3a5d94650f97af7329e37
idlemind <kennmackay.py@gmail.com>
2024-03-19 22:44:07 -0700

    minor updates

--- src/main.rs
+++ src/main.rs
@@ -11,7 +11,6 @@ async fn main() -> Result<(), std::io::Error> {
     init_subscriber(subscriber);
 
     let config = get_configuration().expect("Failed to read configuration.");
-    println!("{:?}", config);
     let connection_pool =
         PgPool::connect_lazy(&config.database.connection_string().expose_secret())
             .expect("Failed to connect to Postgres.");
