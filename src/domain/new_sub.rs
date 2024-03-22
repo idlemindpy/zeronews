48c5ede318c484d9d83512951197416f641b744a
idlemind <kennmackay.py@gmail.com>
2024-03-21 22:36:56 -0700

    More updates to complete chapter 6.

--- src/domain/new_sub.rs
+++ src/domain/new_sub.rs
@@ -1,6 +1,7 @@
+use crate::domain::sub_email::SubscriberEmail;
 use crate::domain::sub_name::SubscriberName;
 
 pub struct NewSubscriber {
-    pub email: String,
+    pub email: SubscriberEmail,
     pub name: SubscriberName,
 }
