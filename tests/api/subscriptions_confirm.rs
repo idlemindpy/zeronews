bfc25dd37719c746c2e7ea74f03c2bb29b46561c
idlemind <kennmackay.py@gmail.com>
2024-03-26 12:45:25 -0700

    7.7.5.1, one red

--- tests/api/subscriptions_confirm.rs
+++ tests/api/subscriptions_confirm.rs
@@ -1,4 +1,7 @@
 use crate::helpers::spawn_app;
+use reqwest::Url;
+use wiremock::matchers::{method, path};
+use wiremock::{Mock, ResponseTemplate};
 
 #[tokio::test]
 async fn confirmation_without_token_are_rejected() {
@@ -9,4 +12,40 @@ async fn confirmation_without_token_are_rejected() {
         .unwrap();
 
     assert_eq!(response.status().as_u16(), 400);
+}
+
+#[tokio::test]
+async fn link_returned_by_subscribe_returns_200() {
+    let app = spawn_app().await;
+    let body = "name=le%20chien&email=cedar%40gmail.com";
+
+    Mock::given(path("/email"))
+        .and(method("POST"))
+        .respond_with(ResponseTemplate::new(200))
+        .mount(&app.email_server)
+        .await;
+
+    app.post_subscriptions(body.into()).await;
+
+    let email_request = &app.email_server.received_requests().await.unwrap()[0];
+    let body: serde_json::Value = serde_json::from_slice(&email_request.body).unwrap();
+
+    let get_link = |s: &str| {
+        let links: Vec<_> = linkify::LinkFinder::new()
+            .links(s)
+            .filter(|l| *l.kind() == linkify::LinkKind::Url)
+            .collect();
+
+        assert_eq!(links.len(), 1);
+        links[0].as_str().to_owned()
+    };
+
+    let raw_confirmation_link = &get_link(&body["HtmlBody"].as_str().unwrap());
+    let confirmation_link = Url::parse(raw_confirmation_link).unwrap();
+
+    assert_eq!(confirmation_link.host_str().unwrap(), "127.0.0.1");
+
+    let response = reqwest::get(confirmation_link).await.unwrap();
+
+    assert_eq!(response.status().as_u16(), 200);
 }
